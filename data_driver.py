fa2ff6ceb71445e39e765fd67b8868596d708d3a
Nurassyl-lab <nurassyl.askar@gmail.com>
2024-10-27 00:11:33 +0800

    Update: Now object acttions could be generated with a fixed direction

--- data_driver.py
+++ data_driver.py
@@ -1,7 +1,7 @@
 # Driver file for the data, datasets, dataloaders, and etc
 
 import argparse
-from grid_maker import GridMaker, plot_matrix
+from grid_maker import GridMaker, plot_grid
 import pandas as pd 
 import numpy as np
 from tqdm import tqdm
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     
     # 'grid_maker' is the data that we will use to test our "main" idea on a smaller scale
     # Data contains 10x10 grid with two objects (1 and 2)
-    if args.data == 'grid_maker':
+    if args.data == 'grid_maker_random_directions':
         # Create a dictionary dataset
         dataset = {
             'grid_size': [],
@@ -48,6 +48,8 @@ if __name__ == '__main__':
             # or
             # Find the path for the objects to not collide
             # randomly generate max_length from 3 to 10
+
+            #TODO: Move visualization part to grid_dataset_visualizer.py
             dataset['grid_size'].append(grid_maker.grid_size)
             if i % 2 == 0:
                 object1_coll_actions, object2_coll_actions = grid_maker.find_path(object1_pos, object2_pos, randomness_factor=0.1)
@@ -59,7 +61,7 @@ if __name__ == '__main__':
                     grid_maker.grid.fill(0)
                     grid_maker.grid[pos_1] = 2
                     grid_maker.grid[pos_2] = 1
-                    plot_matrix(grid_maker.grid, pos_1, pos_1, path=f'./data/grid_dataset_images/sample{i+1}_frame{j}')
+                    plot_grid(grid_maker.grid, pos_1, pos_1, path=f'./data/grid_dataset_images/sample{i+1}_frame{j}')
 
             else:
                 max_length = np.random.randint(3, 10)
@@ -72,7 +74,35 @@ if __name__ == '__main__':
         df = pd.DataFrame(dataset)
         df.to_csv('./data/grid_dataset/grid_dataset.csv', index=False)
 
-    elif args.data == 'some_other_data':
-        pass
+    elif args.data == 'grid_maker_fixed_direction':
+        # Create a dictionary dataset
+        dataset = {
+            'grid_size': [],
+            'object1_action': [],
+            'object2_action': [],
+            'collision_status': []
+            }
+
+        # Create a grid 
+        grid_maker = GridMaker(grid_size=10, min_distance=3)
+        
+        grid_maker.create_grid()
+
+        # use tqdm to display the progress bar
+        for i in tqdm(range(args.num_samples), desc='Generating samples'):
+            # Generate initial positions for the objects 
+            object1_pos, object2_pos = grid_maker.place_rectangles()
+
+            dataset['grid_size'].append(grid_maker.grid_size)
+            
+            object1_coll_actions, object2_coll_actions, collision = grid_maker.fix_direction_action()
+            dataset['collision_status'].append(collision[0])
+            dataset['object1_action'].append(object1_coll_actions)
+            dataset['object2_action'].append(object2_coll_actions)
+
+        # Save the dataset to a csv file
+        df = pd.DataFrame(dataset)
+        df.to_csv('./data/grid_dataset/grid_dataset_fixed_direction.csv', index=False)
+
     else:
         print(f'--data {args.data} is not supported')
---
+++ grid_dataset_visualizer.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import argparse
+from grid_maker import plot_grid
+import numpy as np
+from tqdm import tqdm
+from utils.basic_functions import str2bool
+
+# Load the dataset
+def get_parser():
+    parser = argparse.ArgumentParser(description='Visualize the grid dataset')
+    parser.add_argument('--data_path', type=str, default='./data/grid_dataset/grid_dataset_fixed_direction.csv', help='Path to the data file')
+    parser.add_argument('--num_samples', type=int, default=100, help='Number of samples to generate')
+    parser.add_argument('--collision_only', type=str2bool, default=True, help='Visualize only the samples with collision')
+    return parser
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    data_path = args.data_path
+
+    df = pd.read_csv(data_path)
+
+    num_samples = args.num_samples
+    if num_samples > len(df):
+        num_samples = len(df)
+        print(f'Number of samples requested is greater than the number of samples in the dataset. Setting the number of samples to {num_samples}')
+
+    for i in tqdm(range(len(df)), desc='Visualizing the dataset'):
+        grid_size = df['grid_size'][i]
+        object1_actions = df['object1_action'][i]
+        object2_actions = df['object2_action'][i]
+        collision_status = df['collision_status'][i]
+
+
+        if args.collision_only and collision_status == 0:
+            continue
+
+        object1_actions = eval(object1_actions)
+        object2_actions = eval(object2_actions)
+
+        # create a grid
+        matrix = np.zeros((grid_size, grid_size))
+        for j, (obj1_pos, obj2_pos) in enumerate(zip(object1_actions, object2_actions)):
+            plot_grid(grid_size, obj1_pos, obj2_pos, path=f'./data/grid_dataset_fixed_direction_images/sample{i+1}_frame{j}')
+        
